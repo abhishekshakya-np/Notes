f6b989795da4d3d61e0605209137375787f2fc1b
Abhishek shakya <abishek12344321@gmail.com>
2024-05-24 02:30:12 +0545

    Build a custom PHP router from scratch. Code organization, response codes, and more.

---
+++ notes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+shortcut for above code or refactor
+
+ if ($uri === '/') { 
+    require 'controllers/index.php';
+} else if ($uri === '/about') {
+    require 'controllers/about.php';
+} else if ($uri === '/contact') {  
+    require 'controllers/contact.php';
+} else {
+    echo "No route found";    
+} 
+
+
+ */
