b7a6f427d7bf45b4ef57b3e31dfc4ccfcc1d1f7c
Abhishek shakya <abishek12344321@gmail.com>
2024-05-24 02:45:27 +0545

    Environments and Configuration Flexibility. Push the project to production, where the host and port are entirely different

--- router.php
+++ router.php
@@ -2,14 +2,11 @@
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-
-
 $routes = [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
     '/contact' => 'controllers/contact.php',
 ];
-
 
 function routeToController($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
